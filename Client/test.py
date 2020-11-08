1ff814256518627d1ba19b87c15e00a22c48cb7f
sargaras <sargaras_m@mail.ru>
2020-11-08 13:12:02 +0300

    Обновление

--- Client/test.py
+++ Client/test.py
@@ -3,38 +3,40 @@ import json
 import numpy as np
 import requests
 from Models import *
-obama_image = face_recognition.load_image_file("obama.jpg")
-biden_image = face_recognition.load_image_file("biden.jpg")
+# obama_image = face_recognition.load_image_file("obama.jpg")
+# biden_image = face_recognition.load_image_file("biden.jpg")
+jason_image = face_recognition.load_image_file("jason_statham.jpg")
+jason_image2 = face_recognition.load_image_file("jason_statham2.jpg")
 
 url="https://facerecognitiondbapi20201027214040.azurewebsites.net/employers"
 
 try:
-    face_encoding_obama = face_recognition.face_encodings(obama_image)[0]
-    face_encoding_biden = face_recognition.face_encodings(biden_image)[0]
-  
+    # face_encoding_obama = face_recognition.face_encodings(obama_image)[0]
+    # face_encoding_biden = face_recognition.face_encodings(biden_image)[0]
+    face_encoding_jason=face_recognition.face_encodings(jason_image)[0]
+    face_encoding_jason2=face_recognition.face_encodings(jason_image2)[0]
 except IndexError:
     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
     quit()
 
-
+Data=GetFullData(url)
+DeleteData(url,Data,"Jason Statham")
 
 #добавление данных
-# info_post_1=AddData(url,"Obama",face_encoding_obama.tolist())
-# info_post_2=AddData(url,"Biden",face_encoding_biden.tolist())
+info_post_1=AddData(url,"Jason Statham",face_encoding_jason.tolist())
 
-# print(info_post_1)
-# print(info_post_2)
 
 #получение данных
 Data=GetFullData(url)
 
-results = face_recognition.compare_faces(face_encoding_obama, [Data[0]["features"]])
 
-if (results[0]==True):
-    print(Data[0]["name"])
+for i in (Data):
+    results = face_recognition.compare_faces(face_encoding_jason2, [i["features"]])
 
-#удаление данных
-DeleteData(url,Data,"Yura")
-DeleteData(url,Data,"Obama")
-DeleteData(url,Data,"Biden")
-    
+    if (results[0]==True):
+        print(f'This is a  {i["name"]}')
+
+# #удаление данных
+# DeleteData(url,Data,"Yura")
+# DeleteData(url,Data,"Obama")
+# DeleteData(url,Data,"Biden")
