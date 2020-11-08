f91960b58184aaf61a5e292339a9056033603498
sargaras <sargaras_m@mail.ru>
2020-11-08 22:11:28 +0300

    Добавлен код с добавлением рамки

---
+++ Client/face_detection.py
@@ -0,0 +1,43 @@
+import face_recognition
+import json
+import numpy as np
+import requests
+from Models import *
+import cv2
+
+maksim_image1=face_recognition.load_image_file("Maksim1.jpg")
+maksim_image2=face_recognition.load_image_file("Maksim2.jpg")
+
+url="https://facerecognitiondbapi20201027214040.azurewebsites.net/employers"
+
+try:
+
+    face_encoding_maksim1=face_recognition.face_encodings(maksim_image1)[0]
+    face_encoding_maksim2=face_recognition.face_encodings(maksim_image2)[0]
+except IndexError:
+    print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
+    quit()
+
+results = face_recognition.compare_faces(face_encoding_maksim1,[face_encoding_maksim2])
+
+if (results[0]==True):
+    print(f'This is a Maksim')
+
+photo = cv2.imread("Maksim1.jpg")
+rgb_small_frame = photo[:, :, ::-1]
+face_locations = face_recognition.face_locations(rgb_small_frame)
+
+
+for (top, right, bottom, left) in face_locations:
+    # Scale back up face locations since the frame we detected in was scaled to 1/4 size
+
+    # Draw a box around the face
+    cv2.rectangle(photo, (left, top), (right, bottom), (0, 0, 255), 2)
+
+    # Draw a label with a name below the face
+    cv2.rectangle(photo, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+    font = cv2.FONT_HERSHEY_DUPLEX
+    cv2.putText(photo, 'Maksim', (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+
+cv2.imwrite('Result.jpg',photo)
+cv2.waitKey(0)
--- Client/test.py
+++ Client/test.py
@@ -5,36 +5,46 @@ import requests
 from Models import *
 # obama_image = face_recognition.load_image_file("obama.jpg")
 # biden_image = face_recognition.load_image_file("biden.jpg")
-jason_image = face_recognition.load_image_file("jason_statham.jpg")
-jason_image2 = face_recognition.load_image_file("jason_statham2.jpg")
+# jason_image = face_recognition.load_image_file("jason_statham.jpg")
+# jason_image2 = face_recognition.load_image_file("jason_statham2.jpg")
+
+maksim_image1=face_recognition.load_image_file("Maksim1.jpg")
+maksim_image2=face_recognition.load_image_file("Maksim2.jpg")
 
 url="https://facerecognitiondbapi20201027214040.azurewebsites.net/employers"
 
 try:
     # face_encoding_obama = face_recognition.face_encodings(obama_image)[0]
     # face_encoding_biden = face_recognition.face_encodings(biden_image)[0]
-    face_encoding_jason=face_recognition.face_encodings(jason_image)[0]
-    face_encoding_jason2=face_recognition.face_encodings(jason_image2)[0]
+    # face_encoding_jason=face_recognition.face_encodings(jason_image)[0]
+    # face_encoding_jason2=face_recognition.face_encodings(jason_image2)[0]
+    face_encoding_maksim1=face_recognition.face_encodings(maksim_image1)[0]
+    face_encoding_maksim2=face_recognition.face_encodings(maksim_image2)[0]
 except IndexError:
     print("I wasn't able to locate any faces in at least one of the images. Check the image files. Aborting...")
     quit()
 
-Data=GetFullData(url)
-DeleteData(url,Data,"Jason Statham")
+results = face_recognition.compare_faces(face_encoding_maksim1,[face_encoding_maksim2])
+
+if (results[0]==True):
+    print(f'This is a Maksim')
+
+#подготовка к очистке от старого эксперимента
+#Data=GetFullData(url)
+# DeleteData(url,Data,"Jason Statham")
 
 #добавление данных
-info_post_1=AddData(url,"Jason Statham",face_encoding_jason.tolist())
-
+# info_post_1=AddData(url,"Jason Statham",face_encoding_jason.tolist())
 
 #получение данных
-Data=GetFullData(url)
+#Data=GetFullData(url)
 
 
-for i in (Data):
-    results = face_recognition.compare_faces(face_encoding_jason2, [i["features"]])
+# for i in (Data):
+#     results = face_recognition.compare_faces(face_encoding_jason2, [i["features"]])
 
-    if (results[0]==True):
-        print(f'This is a  {i["name"]}')
+#     if (results[0]==True):
+#         print(f'This is a  {i["name"]}')
 
 # #удаление данных
 # DeleteData(url,Data,"Yura")
